5902da0f5a09730f0c56ac44c2c86b299628baf8
Isaac Lopez Tablet <isaaclopezprog@gmail.com>
2021-09-17 17:46:41 -0400

    Create: carpeta de helpers

---
+++ helpers/inquierer.js
@@ -0,0 +1,98 @@
+const inquirer = require('inquirer');
+require('colors');
+
+const preguntas = (
+  {
+    type: 'list',
+    name:'opcion',
+    message: 'Que desea hacer?',
+    choices: [
+        {
+          value: '1',
+          name:'1. Crear tarea'
+        },
+        {
+          value: '2',
+          name:'2. Listar tareas'
+        },
+        {
+          value: '3',
+          name:'3. Listar tareas completadas'
+        },
+        {
+          value: '4',
+          name:'4. Listar tareas pendientes'
+        },
+        {
+          value: '5',
+          name:'5. Completar tarea(s)'
+        },
+        {
+          value: '6',
+          name:'6. Borrar tarea'
+        },{
+          value: '0',
+          name:'0. Salir'
+        },
+      ]
+  }
+);
+
+const inquirirMenu = async() => {
+  
+    console.clear();
+    console.log('============================'.green);
+    console.log('    Seleccione una opcion'.white);
+    console.log('============================\n'.green);
+    
+    const { opcion } = await inquirer.prompt(preguntas);
+    
+    return opcion;
+    
+}
+
+const enter = (
+  {
+    type: 'input',
+    name:'enter',
+    message: `\n Presione ${ 'ENTER'.green } para continuar \n`,
+  }
+);
+pausa = async () => {
+  const  pausa  = await inquirer.prompt(enter);
+}
+
+
+
+
+
+
+const leerInput = async (message) => {
+  
+  const question = [
+    {
+      type: 'input',
+      name: 'desc',
+      message,
+      validate (value){
+        if(value.length === 0 ){
+          return 'Por favor ingrese un valor';
+        }
+        return true;
+      }
+    }
+    ];
+  
+  
+  const { desc } = await inquirer.prompt(question);
+  return desc;
+}
+
+
+
+
+module.exports = {
+  inquirirMenu,
+  pausa,
+  leerInput
+}
