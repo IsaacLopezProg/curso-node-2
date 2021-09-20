a389181f39d5d854c536243996879780d1982391
Isaac Lopez PC <isaaclopezprog@gmail.com>
2021-09-20 16:34:16 -0400

    se hizo el get de listadoArr

--- models/tareas.js
+++ models/tareas.js
@@ -3,6 +3,16 @@ const Tarea  = require('./tarea');
 class Tareas {
   
   _listado = {};
+
+  get liastadoArr(){
+    const listado = [];
+    Object.keys(this._listado).forEach( key => {
+      const tarea = this._listado[key];
+      listado.push(tarea);
+    });
+
+    return listado;
+  }
   
   constructor(){
     this._listado = {};
@@ -10,7 +20,7 @@ class Tareas {
   
   
   crearTarea( desc = ''){
-    
+     
     const  tarea = new Tarea(desc);
     
     this._listado[tarea.id] = tarea;
