a5c8c8d466cf98533e224393c040174209ab3e28
Isaac Lopez PC <isaaclopezprog@gmail.com>
2021-09-21 19:36:57 -0400

    Se realizo la vista del listado

--- models/tareas.js
+++ models/tareas.js
@@ -36,6 +36,36 @@ class Tareas {
     
     this._listado[tarea.id] = tarea;
   }
+
+
+  listadoCompleto(){
+
+
+    // let li = this._listado;
+
+    
+    // console.log( Object.entries(li));
+    // console.log( Object.values(li));
+    // console.log( Object.keys(li));
+
+
+    console.log();
+
+    this.liastadoArr.forEach( (tarea, i) => {
+
+      const idx = `${i + 1}`.green;
+      const { desc, completadoEn } = tarea;
+      const estado = ( completadoEn )
+                        ? 'Completada'.green
+                        : 'Pendiente'.red;
+
+
+      console.log(`${ idx } ${ desc } :: ${ estado }`);
+
+    });
+
+
+  }
   
   
 }
